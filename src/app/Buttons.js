9758a01e2e45096af6448a3f26b5c76d6af2c8af
Nick <nick.sjostrom12@gmail.com>
2018-02-06 22:34:56 -0500

    add code samples

--- src/app/Buttons.js
+++ src/app/Buttons.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Button from '../components/Button'
+import Button from '../components/Button';
 
 class Buttons extends Component {
 
@@ -21,6 +21,44 @@ class Buttons extends Component {
 					<Button type="danger plain">Danger</Button>	
 					<span></span>
 				</div>
+
+				<br/>
+
+				<h2>React Component</h2>
+<pre>{`
+import Button from 'components/Button';
+
+class App extends Component {
+	render() {
+		<Button>Neutral</Button>	
+		<Button type="success">Success</Button>	
+		<Button type="information">Information</Button>	
+		<Button type="danger">Danger</Button>
+
+		<Button type="plain">Neutral</Button>	
+		<Button type="success plain">Success</Button>	
+		<Button type="information plain">Information</Button>	
+		<Button type="danger plain">Danger</Button>	
+	}
+}
+`}</pre>
+
+
+				<h2>CSS</h2>
+<pre>{`
+<link rel="stylesheet" type="text/css" href="/css/Buttons.css">
+
+<button className="button">Neutral</button>
+<button className="success button">Neutral</button>
+<button className="information button">Neutral</button>
+<button className="danger button">Neutral</button>
+
+<button className="plain button">Neutral</button>
+<button className="success plain button">Neutral</button>
+<button className="information plain button">Neutral</button>
+<button className="danger plain button">Neutral</button>
+
+`}</pre>
 
 			</div>
 		  );
--- src/app/Typography.js
+++ src/app/Typography.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Button from '../components/Button'
 
 class Typography extends Component {
 
