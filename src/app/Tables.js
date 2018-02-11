2db428dae5dc412960876f3cc3db3b879df0c71c
Nick <nick.sjostrom12@gmail.com>
2018-02-10 22:22:28 -0500

    favicon and Table

--- src/app/Tables.js
+++ src/app/Tables.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import Button from '../components/Button';
+import Table from '../components/Table';
 
 class Tables extends Component {
 
 	render() {
+		const headers = [
+			"Name",
+			"Home",
+			"Active",
+			"Age"
+		];
+
+		const values = [
+			["African Aquatic Sideneck Turtle", "Africa", "Daytime", "25 years"],
+			["Central American Wood Turtle", "Mexico, Costa Rica", "", ""],
+			["Painted Turtle", "North America", "Daytime", "55 years"],
+			["Red Ear Slider Turtle", "Gulf of Mexico", "", "50-70 years"],
+			["Reeve’s Turtle", "China, North and South Korea", "", "10-15 years"],
+		]
+
 		return (
 			<div >
 				<h1>Tables</h1>
+
+				<div className="grid grid-1">
+					<Table 
+						classes="hover"
+						headers={headers}
+						values={values}
+					/>
+				</div>
 			</div>
 		  );
 	}
---
+++ src/components/Table.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './Table.css';
+
+
+class Table extends Component {
+
+	constructor(props) {
+	    super(props);
+		this.state = {
+		  classes: props.classes ? props.classes : "",
+		  headers: props.headers ? props.headers : [],
+		  values: props.values ? props.values : []
+		}
+
+	}
+
+	render() {
+		const classes = ["table",this.state.classes,this.state.state].join(" ");
+		return (
+			<table className={classes}>
+				<thead>
+					<tr>
+					{this.state.headers.map(function(header){
+						return (
+							<th>{header}</th>
+						)
+					})}
+					</tr>
+				</thead>
+				<tbody>
+					{this.state.values.map(function(row){
+						return (
+							<tr>
+								{row.map(function(value){
+									return (
+										<td>{value}</td>
+									)
+								})}
+							</tr>
+						)
+					})}
+				</tbody>
+			</table>
+		  );
+	}
+}
+
+
+export default Table;
